c53a356f0065d4b2ffa73fca60d50050ffa837ae
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-06-01 16:51:41 +0200

    Update edit.route.js

--- routes/edit.route.js
+++ routes/edit.route.js
@@ -1,7 +1,6 @@
 const edit= require('express').Router()
 const mongoose=require('mongoose');
 const fileModel = require('../model/file.model');
-mongoose.connect('mongodb://localhost:27017/fileUpload',{useNewUrlParser:true,useUnifiedTopology:true});
 edit.get('/edit/:id',async (req,res)=>{
     let result = await fileModel.find({_id: req.params.id});
     //console.log(result);

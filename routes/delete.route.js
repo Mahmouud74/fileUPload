ffa99e90f635cda6e2d7cd2d2f077bdbb7521ee8
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-06-01 16:51:13 +0200

    Update delete.route.js

--- routes/delete.route.js
+++ routes/delete.route.js
@@ -1,7 +1,6 @@
 const deletee=require('express').Router();
 const mongoose=require('mongoose');
 const fileModel = require('../model/file.model');
-mongoose.connect('mongodb://localhost:27017/fileUpload',{useNewUrlParser:true,useUnifiedTopology:true});
 deletee.get('/delete/:id', async (req,res)=>{
    await fileModel.deleteOne({_id:req.params.id});
    res.redirect('/');   

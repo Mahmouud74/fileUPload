0f6eb30c265767b6c643c0895a99d13fc96ed288
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-06-01 16:52:26 +0200

    Update home.route.js

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,7 +1,6 @@
 const home = require('express').Router()
 const mongoose =require('mongoose');
 const fileModel = require('../model/file.model');
-mongoose.connect('mongodb://localhost:27017/fileUpload',{useNewUrlParser:true , useUnifiedTopology:true})
 home.get('/',async(req,res)=>{
     let result = await fileModel.find()
     res.render('home.ejs',{result});
